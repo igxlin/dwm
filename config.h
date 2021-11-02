30bdc2870128c9e596500dec9002c6018239c61b
Guangxiong Lin <hi@gxlin.org>
2021-11-02 21:03:52 +0800

    Add screenshot command

--- config.h
+++ config.h
@@ -66,11 +66,17 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *fullscreenshotcmd[] = { "scrot", "-e", "mv $f ~/Downloads/", NULL };
+static const char *screenshotcmd[] = { "scrot", "-s", "-e", "mv $f ~/Downloads/", NULL };
+static const char *windowshotcmd[] = { "scrot", "-bs", "-e", "mv $f ~/Downloads/", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = screenshotcmd } },
+	{ MODKEY|ControlMask,           XK_p,      spawn,          {.v = windowshotcmd } },
+	{ MODKEY|ShiftMask,             XK_p,      spawn,          {.v = fullscreenshotcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_Tab,    focusstack,     {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_Tab,    focusstack,     {.i = -1 } },
